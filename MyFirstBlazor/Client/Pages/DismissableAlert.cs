f741f21182e09d6c12aef59155ab250de5aeef37
rob011235 <rgarner011235@gmail.com>
2020-10-12 18:41:23 -0600

    Alert complete

---
+++ MyFirstBlazor/Client/Pages/DismissableAlert.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyFirstBlazor.Client.Pages
+{
+    public partial class DismissableAlert
+    {
+        [Parameter]
+        public bool Show { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        public void Dismiss()
+          => Show = false;
+    }
+}
