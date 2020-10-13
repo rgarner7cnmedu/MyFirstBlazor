de1dfd4e0940f987cf40abb68449c39eb59c8935
rob011235 <rgarner011235@gmail.com>
2020-10-12 19:14:47 -0600

    Two way databinding

--- MyFirstBlazor/Client/Pages/DismissableAlert.cs
+++ MyFirstBlazor/Client/Pages/DismissableAlert.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
 
 namespace MyFirstBlazor.Client.Pages
 {
     public partial class DismissableAlert
     {
+        
+        private bool show;
         [Parameter]
-        public bool Show { get; set; }
+        public bool Show
+        {
+            get { return show; }
+            set 
+            {
+                if(value != this.show)
+                {
+                    this.show = value;
+                    ShowChanged?.Invoke(this.show);
+                }
+                
+            }
+        }
+
+        [Parameter]
+        public Action<bool> ShowChanged { get; set; }
+
         [Parameter]
         public RenderFragment ChildContent { get; set; }
         public void Dismiss()
---
+++ MyFirstBlazor/Client/Pages/Timer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyFirstBlazor.Client.Pages
+{
+    public class Timer:ComponentBase
+    {   
+        [Parameter]
+        public double TimeInSeconds { get; set; }
+
+        [Parameter]
+        public Action Tick { get; set; }
+
+        protected override void OnInitialized()
+        {
+            var timer = new System.Threading.Timer(
+                    callback: (_)=>InvokeAsync(()=>Tick?.Invoke()),
+                    state: null,
+                    dueTime: TimeSpan.FromSeconds(TimeInSeconds),
+                    period: Timeout.InfiniteTimeSpan
+                );
+        }
+    }
+}
