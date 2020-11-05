7ec8e7bca89fa3bd83d4d81e7d488e78c53f5967
rob011235 <rgarner011235@gmail.com>
2020-11-04 17:40:54 -0700

    Fix extensions error.

--- MyFirstBlazor/Shared/DependencyInjection.cs
+++ MyFirstBlazor/Shared/DependencyInjection.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿//using Microsoft.Extensions.DependencyInjection;
 using MyFirstBlazor.Client.Services;
 
 namespace BlazorLifetime.Shared
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddLifetime(
-          this IServiceCollection services)
-        {
-            services.AddSingleton<SingletonService>();
-            services.AddTransient<TransientService>();
-            services.AddScoped<ScopedService>();
-            return services;
-        }
+        //public static IServiceCollection AddLifetime(
+        //  this IServiceCollection services)
+        //{
+        //    services.AddSingleton<SingletonService>();
+        //    services.AddTransient<TransientService>();
+        //    services.AddScoped<ScopedService>();
+        //    return services;
+        //}
     }
 }
